6f556167e13b1a9601a46a0ca697632fb9014797
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-12-16 18:05:07 -0800

    tables made in migrations

--- data/migrations/20211217015344_01-tables.js
+++ data/migrations/20211217015344_01-tables.js
@@ -1,8 +1,25 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (table) => {
+      table.increments("user_id");
+      table.string("name", 255).unique().notNullable();
+      table.string("email", 255).unique().notNullable();
+      table.string("password", 255).notNullable();
+    })
+    .createTable("posts", (table) => {
+      table.increments("post_id");
+      table.string("post_title").notNullable();
+      table.string("photo_url");
 
-exports.up = function(knex) {
-  
+      table
+        .integer("user_id")
+        .usigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("posts").dropTableIfExists("users");
 };
